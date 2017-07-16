f7fa07bb63f21e6a7783bd67ce842578feeb2057
SR_team <sr-tream@yandex.ru>
2017-07-16 21:43:21 +0300

    Create header

--- codegenerator.h
+++ codegenerator.h
@@ -1,11 +1,38 @@
 #ifndef CODEGENERATOR_H
 #define CODEGENERATOR_H
 
+#include <QFile>
+#include <QStringList>
 
 class CodeGenerator
 {
 public:
-    CodeGenerator();
+    CodeGenerator(QString filePath, bool useSpaces = 1);
+
+    QStringList generateCode(QString NS, QString gNS);
+
+    void setUseSpaces(bool state);
+    bool isUseSpaces();
+    void setSpacesInTab(int count);
+    int spacesInTab();
+    void setCols(int count);
+    int cols();
+
+protected:
+    void setOffset(uint tabs);
+    uint offset();
+
+    void writeString(QString);
+
+private:
+    QString filePath;
+    QFile *file = nullptr;
+    QByteArray fileArray;
+    bool useSpaces;
+    uint codeOffset = 0;
+    int spacesInOneTab = 4;
+    int octets = 12;
+    QStringList code;
 };
 
 #endif // CODEGENERATOR_H
